428ea968dfa6f8ccdaddda8a868e382c2f1b5cdb
hunterdyar <bloop@hdyar.com>
2024-06-17 12:37:30 -0400

    nonwork

--- dice-results-viz/Dice/RollCodeParser/DiceCodeParserSprache.cs
+++ dice-results-viz/Dice/RollCodeParser/DiceCodeParserSprache.cs
@@ -10,10 +10,18 @@ public class DiceCodeParserSprache
 		from x in Parse.Number
 		select new NumberExpression(int.Parse(x));
 
-	public static readonly Parser<DiceRollExpression> NormalDiceRoll =
+	public static readonly Parser<Expression> NormalDiceRoll =
 		from d in Number
 		from sep in DiceSep
 		from f in Number
+		//label
 		select new DiceRollExpression(d,f);
-	
+
+	public static readonly Parser<Expression> DiceOrNumber =
+		from d in NormalDiceRoll.Or(Number)
+		select d;
+	// public static readonly Parser<ExpressionGroup> ExpressionGroup =
+	// 	from e in NormalDiceRoll.Many()
+	// 	select new ExpressionGroup(e.GetEnumerator());
+
 }
